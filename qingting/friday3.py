0928acedf0f52747ffb9cae0880634b10d9490dd
unknown <wangxingyu@jiuqi.com.cn>
2019-04-19 23:50:05 +0800

    commit

--- qingting/friday3.py
+++ qingting/friday3.py
@@ -1,10 +1,13 @@
 # -*- coding: UTF-8 -*-
 
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import time, requests, sys, logging, os, random
 
+driver = None
+
 def main(start, end):
-    sleep_time = 60 * 12   # 每次下载的间隔时间
+    sleep_time = 60 * 5   # 每次下载的间隔时间
 
     logger = logging.getLogger('friday')
     logger.setLevel(logging.DEBUG)
@@ -23,6 +26,22 @@ def main(start, end):
 
     download_with_request(start, end, logger, url_pattern, sleep_time)
 
+def download_with_selenium(start, end, logger, url_pattern, sleep_time):
+    for page in range(start, end + 1):
+        if page == 0:
+            continue
+
+        file = './.res/friday/{}.m4a'.format(page)
+        if os.path.isfile(file):
+            size = os.path.getsize(file)
+            if size > 1024 * 1024:
+                logger.debug('文件{}已存在，不重复下载'.format(file))
+                continue
+            else:
+                logger.debug('文件{}已存在，但文件小于1M，重复下载文件'.format(file))
+
+        
+
 def download_with_request(start, end, logger, url_pattern, sleep_time):
     for page in range(start, end + 1):
         if page == 0:
@@ -37,7 +56,9 @@ def download_with_request(start, end, logger, url_pattern, sleep_time):
             else:
                 logger.debug('文件{}已存在，但文件小于1M，重复下载文件'.format(file))
 
-        content = download_single(page, url_pattern, logger)
+        # content = parse_by_requests(page, url_pattern, logger)
+        start = int(time.time())
+        content = parse_by_selenium(page, url_pattern, logger)
 
         # if len(content) < 1024 * 1024:
         #     # repeat once
@@ -47,17 +68,52 @@ def download_with_request(start, end, logger, url_pattern, sleep_time):
         #     time.sleep(st)
         #     content = download_single(page, url_pattern, logger)
 
+        while len(content) > 10000 and len(content) < 15000:
+            # pc 端
+            time.sleep(5)
+            logger.debug('\tpc端返回"推荐手机访问"，重试')
+            content = parse_by_selenium(page, url_pattern, logger)
+
         if len(content) < 1024 * 1024:
             logger.warning('下载的文件重试后依然小于1M，已保存至{}，继续后面的下载'.format(file))
 
         with open(file, 'wb') as writer:
             writer.write(content)
 
-        st = sleep_time + random.randint(-60, 60)
-        logger.debug('延时{}秒'.format(st))
-        time.sleep(st)
+        end = int(time.time())
+        st = sleep_time + random.randint(0, 60)
 
-def download_single(page, url_pattern, logger):
+        duration = st - (end - start)
+
+        if duration > 0:
+            logger.debug('延时{}秒'.format(duration))
+            time.sleep(duration)
+
+def parse_by_selenium(page, url_pattern, logger):
+    # //*[@id="wrapper"]/div/div/div/div[2]/div[1]/audio/source
+    user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
+    user_agent = 'Mozilla/5.0 (Linux; Android 8.1.0; OE106 Build/OPM1.171019.026) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.70 Mobile Safari/537.36'
+    global driver
+    if driver is None:
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        # option.add_argument('--user-agent={}'.format(user_agent))
+        driver = webdriver.Chrome(chrome_options=option)
+
+    url = url_pattern.format(page)
+    logger.info('开始解析第{}章，url:{}'.format(page, url))
+    driver.get(url)
+    xpath = '//*[@id="wrapper"]/div/div/div/div[2]/div[1]/audio/source' # pc
+    # xpath = '//*[@id="player"]/source'  # mobile
+    time.sleep(5)
+    src = driver.find_element_by_xpath(xpath).get_attribute('src')
+    logger.debug('识别到文件下载路径:{}'.format(src))
+
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+
+    return download_m4a(src, logger, page, url, user_agent)
+
+def parse_by_requests(page, url_pattern, logger):
     url = url_pattern.format(page)
     logger.info('开始下载第{}章，url:{}'.format(page, url))
     user_agent = 'Mozilla/5.0 (Linux; Android 8.1.0; OE106 Build/OPM1.171019.026) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.70 Mobile Safari/537.36'
@@ -106,5 +162,5 @@ def download_m4a(src, logger, page, Referer, user_agent):
 
 
 if __name__ == "__main__":
-    main(230, 660)
+    main(130, 660)
 
